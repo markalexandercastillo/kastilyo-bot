9438647ec63d5cecb48db59c1b7af8ac50cec187
Mark Castillo <markalexandercastillo@gmail.com>
2017-03-10 14:59:50 -0500

    Using whole hash fetch

--- src/services/congress/bills/index.js
+++ src/services/congress/bills/index.js
@@ -5,18 +5,17 @@ const _ = require('lodash')
   , cache = require('./../../cache').create('kastilyo-bot', 'congress', 'bills')
   , members = require('./../members');
 
-function getDataPromises(id) {
-  const cacheHashKey = `bill-${slugifyId(id)}`;
-  return [
-    cache.hash.fetchField(cacheHashKey, 'data', () => data.get(id)),
-    cache.hash.fetchField(cacheHashKey, 'subjects', () => data.getSubjects(id)),
-    cache.hash.fetchField(cacheHashKey, 'amendments', () => data.getAmendments(id)),
-    cache.hash.fetchField(cacheHashKey, 'cosponsors', () => data.getCosponsorIds(id).map(members.get))
-  ];
+function getData(id) {
+  return cache.hash.fetch(`bill-${slugifyId(id)}`, {
+    data: () => data.get(id),
+    subjects: () => data.getSubjects(id),
+    amendments: () => data.getAmendments(id),
+    cosponsors: () => data.getCosponsorIds(id).map(members.get)
+  });
 }
 
-function mergeData([bill, subjects, amendments, cosponsors]) {
-  return Object.assign(bill, {
+function mergeData({data, subjects, amendments, cosponsors}) {
+  return Object.assign(data, {
     subjects,
     amendments,
     cosponsors
@@ -24,16 +23,12 @@ function mergeData([bill, subjects, amendments, cosponsors]) {
 }
 
 function getOne(id) {
-  return Promise.all(getDataPromises(id))
+  return getData(id)
     .then(mergeData);
 }
 
 function getMany(ids) {
-  return Promise.all(ids.map(getDataPromises).reduce(
-      (accumulatedPromises, dataPromises) => [...accumulatedPromises, ...dataPromises],
-      []
-    ))
-    .then(billDataComponents =>_.chunk(billDataComponents, 4))
+  return Promise.all(ids.map(getData))
     .map(mergeData);
 }
 

e4a53a639f81589711bd6d0cdb4e9fc82976ccae
Mark Castillo <markalexandercastillo@gmail.com>
2017-03-11 01:46:50 -0500

    Refactoring how to make this a bluebird promise
    Wasn't sure how an error from getMemberList would work with promise.resolve

--- src/services/congress/members/data.js
+++ src/services/congress/members/data.js
@@ -14,12 +14,10 @@ module.exports = {
     );
   },
   getIdList(chamber, offset = 0) {
-    return new Promise(
-      (resolve, reject) => Promise.resolve(ppc.getMemberList(chamber, {offset}))
-        .then(data => data.results[0].members)
-        .map(member =>  member.id)
-        .then(resolve)
+    return (new Promise(
+      (resolve, reject) => ppc.getMemberList(chamber, {offset})
+        .then(data => resolve(data.results[0].members))
         .catch(reject)
-    );
+    )).map(member =>  member.id);
   }
 };

c7435df7e0610b1b09a99941fd1b18b4dbc1343a
Mark Castillo <markalexandercastillo@gmail.com>
2017-03-11 02:53:30 -0500

    Hopefully establshing a convention for extending bot behavior

--- src/bot/index.js
+++ src/bot/index.js
@@ -1,12 +1,4 @@
 const bot = require('./../helpers/telegram');
-const tc = require('./../toon-caps');
+const tc = require('./toon-caps');
 
-bot.onText(
-  /\/(morbotron|frinkiac) (.+)/,
-  ({message_id, chat}, [ignore, toonCapsKey, searchTerms]) =>
-      tc[toonCapsKey].findRandomMeme(searchTerms)
-        .then(({meme, episode}) => bot.sendPhoto(chat.id, meme.imageUrl, {
-          caption: episode.wikiLink,
-          reply_to_message_id: message_id
-        }))
-);
+tc.extend(bot);

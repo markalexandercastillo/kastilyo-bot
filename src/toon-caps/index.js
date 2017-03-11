38819d4dea454b15f880cb633c63f858f6d5d9c0
Mark Castillo <markalexandercastillo@gmail.com>
2017-03-11 02:46:11 -0500

    Hopefully justifying what's happening here

--- src/toon-caps/index.js
+++ src/toon-caps/index.js
@@ -4,6 +4,8 @@ const _ = require('lodash')
 
 const {assign} = _;
 
+// morbotron and frinkiac are getters on tc so it looks like I can't just assign to those keys.
+// instead i'm exporting their extended return values.
 module.exports = ['morbotron', 'frinkiac']
   .reduce((extendedTc, key) => assign(extendedTc, {
     [key]: assign(tc[key], {

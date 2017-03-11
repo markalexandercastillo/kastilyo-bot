85535e15c281c6ed28fd64e2ea9d894c34fd0eb9
Mark Castillo <markalexandercastillo@gmail.com>
2017-03-11 03:34:34 -0500

    Extracting handler to further establish intended convention

--- src/bot/toon-caps.js
+++ src/bot/toon-caps.js
@@ -1,14 +1,17 @@
 const tc = require('./../toon-caps');
 
+const findRandomMeme =
+  (bot, {message_id, chat}, [ignore, toonCapsKey, searchTerms]) =>
+    tc[toonCapsKey].findRandomMeme(searchTerms)
+      .then(({meme, episode}) => bot.sendPhoto(chat.id, meme.imageUrl, {
+        caption: episode.wikiLink,
+        reply_to_message_id: message_id
+      }));
+
 function extend(bot) {
   bot.onText(
     /\/(morbotron|frinkiac) (.+)/,
-    ({message_id, chat}, [ignore, toonCapsKey, searchTerms]) =>
-        tc[toonCapsKey].findRandomMeme(searchTerms)
-          .then(({meme, episode}) => bot.sendPhoto(chat.id, meme.imageUrl, {
-            caption: episode.wikiLink,
-            reply_to_message_id: message_id
-          }))
+    (message, matches) => findRandomMeme(bot, message, matches)
   );
 }
 

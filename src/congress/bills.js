72410f102958e94bcf7bb62803c422b43c4a6653
Mark Castillo <markalexandercastillo@gmail.com>
2017-03-19 08:25:33 -0400

    Compacting by getting rid of explicit getMany functions and other single-use functions that seemed to only spread logic around
    Also getting members to look like bills since there will most likely be more data fragments to be set

--- src/congress/bills.js
+++ src/congress/bills.js
@@ -1,44 +1,31 @@
-const _ = require('lodash')
-  , Promise = require('bluebird')
+const {assign, isArray} = require('lodash')
+  , {all} = require('bluebird')
   , ppc = require('./propublica-congress')
   , cache = require('./../cache').create('kastilyo-bot', 'congress', 'bills')
   , members = require('./members');
 
-function getData(id) {
+function getOne(id) {
   return cache.hash.fetch(`bill-${ppc.normalizeBillId(id)}`, {
     data: () => ppc.getBill(id),
     subjects: () => ppc.getAdditionalBillDetails(id, 'subjects'),
     amendments: () => ppc.getAdditionalBillDetails(id, 'amendments'),
     cosponsors: () => ppc.getAdditionalBillDetails(id, 'cosponsors').map(({cosponsor_id}) => members.get(cosponsor_id))
-  });
-}
-
-function mergeData({data, subjects, amendments, cosponsors}) {
-  return Object.assign(data, {
+  }).then(({data, subjects, amendments, cosponsors}) =>  assign(data, {
     subjects,
     amendments,
     cosponsors
-  });
-}
-
-function getOne(id) {
-  return getData(id)
-    .then(mergeData);
-}
-
-function getMany(ids) {
-  return Promise.all(ids.map(getOne));
+  }));
 }
 
 function get(idOrIds) {
-  return _.isArray(idOrIds) ? getMany(idOrIds) : getOne(idOrIds);
+  return isArray(idOrIds) ? all(idOrIds.map(getOne)) : getOne(idOrIds);
 }
 
 function getRecent(chamber, type, offset = 0) {
   return cache.list.fetch(
     `bills-${type}-${chamber}-${offset}`,
     () => ppc.getRecentBillIds(chamber, type, offset)
-  ).then(getMany);
+  ).then(get);
 }
 
 module.exports = {
--- src/congress/members.js
+++ src/congress/members.js
@@ -1,11 +1,12 @@
-const _ = require('lodash')
-  , Promise = require('bluebird')
+const {isArray, assign} = require('lodash')
+  , {all} = require('bluebird')
   , cache = require('./../cache').create('kastilyo-bot', 'congress', 'members')
   , ppc = require('./propublica-congress');
 
 function getOne(id) {
-  const key = `member-${id}`;
-  return cache.hash.fetchField(key, 'data', () => ppc.getMember(id));
+  return cache.hash.fetch(`member-${id}`, {
+    data: () => ppc.getMember(id)
+  }).then(({data}) => assign(data, {}));
 }
 
 function getList(chamber, offset = 0) {
@@ -14,12 +15,8 @@ function getList(chamber, offset = 0) {
     .map(getOne);
 }
 
-function getMany(ids) {
-  return Promise.all(ids.map(getOne));
-}
-
 function get(idOrIds) {
-  return _.isArray(idOrIds) ? getMany(idOrIds) : getOne(idOrIds);
+  return isArray(idOrIds) ? all(idOrIds.map(getOne)) : getOne(idOrIds);
 }
 
 module.exports = {

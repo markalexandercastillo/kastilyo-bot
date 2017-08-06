e05612268ba624125e3ec63c83e82dcca1b50762
Mark Castillo <markalexandercastillo@gmail.com>
2017-08-06 17:41:33 -0400

    Adding rick and morty support

--- src/bot/toon-caps.js
+++ src/bot/toon-caps.js
@@ -2,11 +2,18 @@ const tc = require('./../toon-caps');
 
 function extend({command$, bot}) {
   const tcCommand$ = command$
-    .filter(({type, args}) => (type === 'morbotron' || type === 'frinkiac') && args.length);
+    .filter(({type, args}) =>
+      (
+        type === 'morbotron'
+        || type === 'frinkiac'
+        || type === 'infinite_rick'
+      ) && args.length);
+
+  const resolveClient = type => tc[type === 'infinite_rick' ? 'masterOfAllScience' : type];
 
   tcCommand$.onValue(
     ({type, args, message}) =>
-        tc[type].findRandomMeme(args.join(' '))
+        resolveClient(type).findRandomMeme(args.join(' '))
           .then(({meme, episode}) => bot.sendPhoto(message.chat.id, meme.imageUrl, {
             caption: episode.wikiLink,
             reply_to_message_id: message.message_id
--- src/toon-caps/index.js
+++ src/toon-caps/index.js
@@ -6,7 +6,7 @@ const {assign} = _;
 
 // morbotron and frinkiac are getters on tc so it looks like I can't just assign to those keys.
 // instead i'm exporting their extended return values.
-module.exports = ['morbotron', 'frinkiac']
+module.exports = ['morbotron', 'frinkiac', 'masterOfAllScience']
   .reduce((extendedTc, key) => assign(extendedTc, {
     [key]: assign(tc[key], {
       findRandomMeme
